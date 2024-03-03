182e3ad65334cdad9b3bb9efbc0d679d2daee508
syful2021 <mdsyfulislam20009@gmail.com>
2024-03-03 15:15:01 +0600

    partners image sent to database

--- admin/placementpartner.php
+++ admin/placementpartner.php
@@ -5,24 +5,25 @@
      header('location: index.php');
   }
 
-  if(isset($_POST['save_image'])){
-
-    $file_name= $_FILES['image']['name'];
-    $random = rand(000,999);
-    $random = str_pad($random, 3, '0', STR_PAD_LEFT);
-    $file_name = $random.$file_name;
-    $tmp_name= $_FILES['save_image']['tmp_name'];
-
-
-    $sql = "INSERT INTO `partners`(`image`) VALUES ('$file_name')";
-    $query = mysqli_query($conn, $sql);
-    if($query){
-        move_uploaded_file($tmp_name,"upload/partners/".$file_name);
-        header('location: placementpartner.php');
-    }else{
-        $failed_data = "data instert faild";
+    if(isset($_POST['save_image'])){
+  
+        $file_name= $_FILES['image']['name'];
+        $random = rand(000,999);
+        $random = str_pad($random, 3, '0', STR_PAD_LEFT);
+        $file_name = $random.$file_name;
+        $tmp_name= $_FILES['image']['tmp_name'];
+  
+  
+        $sql = "INSERT INTO `partners`(`image`) VALUES ('$file_name')";
+        $query = mysqli_query($conn, $sql);
+        if($query){
+            move_uploaded_file($tmp_name,"upload/partners".$file_name);
+            header('location: placementpartner.php');
+        }else{
+            $failed_data = "data instert faild";
+        }
     }
-}
+
 ?>
 
 <!DOCTYPE html>
@@ -135,7 +136,7 @@
                     <div class="card-body m-5">
                         <form action="placementpartner.php" method="POST" enctype="multipart/form-data">
                             <div class="form-group  mb-3">
-                                <label for="">Partners Image</label>
+                                <label  class="form-label" for="image">Partners Image</label>
                                 <input type="file" name="image" class="form-control">
                             </div>
                             <div class="form-group  mb-3">
